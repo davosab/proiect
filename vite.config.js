1f0ea9b78d4b095a30e32b590e87ad3b9ccca7f8
davosab <davidsabau72@gmail.com>
2025-11-12 17:49:08 +0200

    again

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/proiect",
+  base: "/proiect/",
   plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
